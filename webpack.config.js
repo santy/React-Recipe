e338e7e5d3567bae68579347d05dafa6ef8fdcda
Santy Cierco <santidisturbios@gmail.com>
2023-06-22 17:00:53 +0200

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+// ./webpack.config.js
+
+var path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "dist", "assets"),
+    filename: "./bundle.js"
+  },
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
+  }
+};
